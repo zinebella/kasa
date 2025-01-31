0c1e4586f8df0c81684b7cd7943238bf8aa833d0
zineb <zineb.elhande@gmail.com>
2025-02-01 00:28:14 +0100

    modifications header navbar & Banner

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import Logo from '../Assets/logo.png';
-import '../Styles/Header.css';
-import { NavLink } from 'react-router-dom';
-import Navbar from './Navbar';
+import React from "react";
+import Logo from "../Assets/logo.png";
+import "../Styles/Header.css";
+import Nav from "./Navbar";
 
 function Header() {
   return (
     <header className="header">
-      <NavLink to="/">
-        <div>
+        <figure className="header_figure">
           <img className="logo" src={Logo} alt="kasa,location d'appartements" />
-        </div>
-      </NavLink>
-      <Navbar/>
+        </figure>
+      <Nav className="navbar"/>
     </header>
   )
 }
--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import '../Styles/Navbar.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../Styles/Navbar.css"
 
-function Navbar() {
+function Nav() {
   return (
     <nav className="navbar">
-      <NavLink to="/">
+      <NavLink to="/" className="navbar_navLink_home">
         <div>Accueil</div>
       </NavLink>
-      <NavLink to="/about">
+      <NavLink to="/about" className="navbar_navLink_about">
         <div> A propos</div>
       </NavLink>
     </nav> 
   )
 }
 
-export default Navbar
+export default Nav
