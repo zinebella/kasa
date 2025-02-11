787fc6d35fc1944178afbd76c9492bc97f8f08bd
zineb <zineb.elhande@gmail.com>
2025-02-11 23:04:36 +0100

    suppression des lignes vides inutiles

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,18 +4,12 @@ import Banner from "../Compenents/Banner";
 import Cards from "../Compenents/Cards";
 
 
-
-
 function Home () {
-    
     return (
            <div className="home">
               <Banner/>
               <Cards/>
            </div>
-           
-           
-        
     )
 }
 export default Home;
